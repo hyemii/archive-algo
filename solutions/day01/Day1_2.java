4b378a7762c4fec2c973e1a9c6251456d59e99c0
hyemii <30998634+hyemii@users.noreply.github.com>
2026-03-13 19:47:35 +0900

    feat: Day 1 Problem 2 풀이 템플릿 추가

---
+++ solutions/day01/Day1_2.java
@@ -0,0 +1,15 @@
+// [코테] 최장 증가 부분 수열 개수 - DP + 이분탐색
+// Day 1 - Problem 2 | 2026.03.13
+// 알고리즘 유형: 동적계획법/LIS
+
+import java.util.*;
+
+public class Day1_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: 풀이 작성
+
+        scanner.close();
+    }
+}
