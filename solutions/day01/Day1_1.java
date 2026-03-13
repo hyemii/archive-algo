f56dd2cf80182833a387e72967579fc6bd153a38
hyemii <30998634+hyemii@users.noreply.github.com>
2026-03-13 19:47:25 +0900

    feat: Day 1 Problem 1 풀이 템플릿 추가

---
+++ solutions/day01/Day1_1.java
@@ -0,0 +1,15 @@
+// [코테] 슬라이딩 윈도우 최댓값 - 덱을 이용한 최적화
+// Day 1 - Problem 1 | 2026.03.13
+// 알고리즘 유형: 큐/덱
+
+import java.util.*;
+
+public class Day1_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: 풀이 작성
+
+        scanner.close();
+    }
+}
