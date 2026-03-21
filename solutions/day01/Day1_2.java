1aa48304e9c29928e3375b0e97559e692284d76c
hyemii <30998634+hyemii@users.noreply.github.com>
2026-03-21 14:09:25 +0900

    feat: Day 1 Problem 2 풀이 템플릿 추가

---
+++ solutions/day01/Day1_2.java
@@ -0,0 +1,15 @@
+// [코테] 세 수의 합 최솟값 찾기 - 투 포인터
+// Day 1 - Problem 2 | 2026.03.21
+// 알고리즘 유형: 투 포인터/정렬
+
+import java.util.*;
+
+public class Day1_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: 풀이 작성
+
+        scanner.close();
+    }
+}
