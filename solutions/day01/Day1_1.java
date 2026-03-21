e0fa81238c86b99a3c1d5e481fedd5bc29aa250a
hyemii <30998634+hyemii@users.noreply.github.com>
2026-03-21 14:09:18 +0900

    feat: Day 1 Problem 1 풀이 템플릿 추가

---
+++ solutions/day01/Day1_1.java
@@ -0,0 +1,15 @@
+// [코테] 부분 문자열의 최대 길이 - 중복 문자 제거
+// Day 1 - Problem 1 | 2026.03.21
+// 알고리즘 유형: 해시맵/슬라이딩 윈도우
+
+import java.util.*;
+
+public class Day1_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: 풀이 작성
+
+        scanner.close();
+    }
+}
