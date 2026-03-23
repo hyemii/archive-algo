187636ba999139021db05d398deaa6c6cd4515ab
hyemii <30998634+hyemii@users.noreply.github.com>
2026-03-23 10:03:17 +0900

    feat: Day 3 Problem 1 풀이 템플릿 추가

---
+++ solutions/day03/Day3_1.java
@@ -0,0 +1,15 @@
+// [코테] 프로젝트 선택 최적화 - 2D DP
+// Day 3 - Problem 1 | 2026.03.23
+// 알고리즘 유형: 동적계획법/배낭문제
+
+import java.util.*;
+
+public class Day3_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: 풀이 작성
+
+        scanner.close();
+    }
+}
