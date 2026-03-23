4028d54324ace30ff32625cf1aa0939bbb3390bc
hyemii <30998634+hyemii@users.noreply.github.com>
2026-03-23 10:03:27 +0900

    feat: Day 3 Problem 2 풀이 템플릿 추가

---
+++ solutions/day03/Day3_2.java
@@ -0,0 +1,15 @@
+// [코테] 미로 탈출 경로 개수 - DFS 백트래킹
+// Day 3 - Problem 2 | 2026.03.23
+// 알고리즘 유형: 그래프/DFS 백트래킹
+
+import java.util.*;
+
+public class Day3_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: 풀이 작성
+
+        scanner.close();
+    }
+}
